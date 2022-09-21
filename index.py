a57d18ff97481f1e2b56c4edbe213e76e3e67f16
golemiam <52841560+golemiam@users.noreply.github.com>
2022-09-20 20:38:46 -0600

    Update index.py
    Added collaborated project from Google Docs

--- index.py
+++ index.py
@@ -1,80 +1,41 @@
-Write a program to prompt the user for the following:
-First name
-Last name
-Email Address
-Phone Number
-Job Title
-ID Number
-Then you should display the information back in this format:
-Note that the square brackets [] indicate a value coming from the user and should not be displayed.
+"""
+CSE110 Module 2 Team assignment
+Slack Channel: #rock-cse110-fall2022
+Creators: Robbie Platt and Robert Boyd 
+Date: Sept. 20, 2022
+"""
 
 
-----------------------------------------
-[LAST NAME], [first name]
-[Job title]
-ID: [id number]
+def main():
+    first_name = input("What is your first name:  ")
+    last_name = input("What is your last name: ")
+    job_title = input("What is your job title? ")
+    id = input("What is your id number? ")
+    email = input("What is your email? ")
+    phone = input("What is your phone number? ")
+    hair = input("What is your hair color? ")
+    eyes = input("What is your eye color? ")
+    month = input("What is your birth month?: ")
+    license = input("Are you licensed? If yes what kind of license?: ")
 
-[email address]
-[phone number]
-----------------------------------------
-
-In addition to the spacing and punctuation shown above, you must implement the following requirements:
-The last name should be converted from whatever the user types to be displayed in ALL CAPS.
-The job title should be displayed so that the first letter is capitalized.
-The email address should be displayed in all lowercase.
-An example of the program running is shown:
-  
-Please enter the following information:
-
-First name: Jane
-Last name: Doe
-Email address: JaneDoe@email.com
-Phone number: (208) 555-1234
-Job title: chief software architect
-ID Number: 83-23821
-
-The ID Card is:
-----------------------------------------
-DOE, Jane
-Chief Software Architect
-ID: 83-23821
-
-janedoe@email.com
-(208) 555-1234
-----------------------------------------
+    capped_last = last_name.upper()
+    cap_job = job_title.capitalize()
+    email_lower = email.lower()
 
 
-CORE REQUIREMENTS
-Each team activity will contain three core requirements. These are items that you are expected to be able to complete during the one hour team meeting if you have come prepared.
-You should work through the assignment in this order:
-Prompt for all of the necessary values and store them in variables. Then display each item to the screen, without worrying about any spacing, punctuation, or formatting yet.
-Arrange the display so that the spacing and punctuation exactly match the example shown.
-Use the built-in string functions to make the last name display in all caps, the job title display in "title case," and the email display in all lowercase.
-STRETCH CHALLENGE
-Most team activities will also contain stretch challenges, which are not specifically required by the assignment, but are a great way to dive deeper into the material. They can be more difficult and may require you to find solutions that weren't directly covered in the preparation material.
-The stretch challenges for this activity are:
-Add hair color and eye color and put them both on the same line in the display.
-Add a field for the name of the month they started and also a yes/no field for whether they have completed advanced training. Put these both on the same line in the display.
-For the two lines that you just added, make it so that the second columns align, regardless of how many letters are in the responses.
-To complete this one, you may need to search the internet for something like, "python spacing format" or something similar to see if you get any ideas.
-At the end of the stretch challenge, your output should look something like the following:
-The ID Card is:
-----------------------------------------
-DOE, Jane
-Chief Software Architect
-ID: 83-23821
+    print(f"""
+    ----------------------------------------
+    {capped_last}, {first_name}
+    {cap_job}
+    ID: {id}
 
-janedoe@email.com
-(208) 555-1234
+    {email}
+    {phone}
 
-Hair: Brown           Eyes: Blue
-Month: September      Training: Yes
-----------------------------------------
-Sample Solution
-When your program is finished, please view the sample solution for this program to compare it to your approach.
-You should work to complete this activity for the one hour period first, without looking at the sample solution. However, if you have worked on it for at least an hour and are still having problems, you may feel free to use the sample solution to help you finish your program.
-Sample solution (Core Requirements): teach02_sample.py
-Sample solution (Stretch Challenges): teach02_stretch_sample.py
-Submission
-When complete, please report your progress in the associated I-Learn quiz.
-If you decided to do additional work on the program after your team activity, either by yourself or with others, feel free to include that additional work when you report on your progress in I-Learn.
+    Hair: {hair}           Eyes: {eyes}
+    Month: {month}      License Type: {license}
+    ----------------------------------------
+    """)
+
+if __name__== "__main__":
+    main()
